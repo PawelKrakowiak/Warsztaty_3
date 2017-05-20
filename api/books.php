ce018ad6f8c8cd4055c9b982ff7b68e57099b644
Paweł Krakowiak <krakowiakp@gmail.com>
2017-05-20 13:25:27 +0200

    added parameter id checking

--- api/books.php
+++ api/books.php
@@ -2,6 +2,14 @@
 require_once 'src/connection.php';
 require_once 'src/book.php';
 
-$books = Book::loadAllBooksFromDB($connection);
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $book = Book::loadBookFromDB($connection, $id);
+        echo json_encode($book);
+    }else{
+        $books = Book::loadAllBooksFromDB($connection);
+        echo json_encode($books);
+    }
+}
 
-echo json_encode($books);
